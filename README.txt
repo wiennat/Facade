d57fde1b089aa93859cd73808536150921b1f6ca
Wiennat Mongkulmann <wiennat@gmail.com>
2010-09-05 11:59:20 +0900

    Initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Facade
+======
+
+Install
+-------
+In order to use facade, you need Facebook PHP SDK.
+You also need to config the app id and app secret in config.php.
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('FACEBOOK_APP_ID', '142993149051614');
-define('FACEBOOK_APP_SECRET', '50fd55bb8b671e105380a98a7cf7936d');
-define('BASE_URL', 'http://gme3ujaa.joyent.us/facade66/');
+define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_APP_SECRET', '');
+define('BASE_URL', '');
 
 $scope = "publish_stream,user_photos,friends_photos,user_status,friends_status,user_videos,friends_videos";
 require('facebook-php-sdk/src/facebook.php');
